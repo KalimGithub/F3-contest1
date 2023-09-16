44f250b4fd5076c8f97a03fd0a590ec5e6f069a5
KalimGithub <sk22kalim@gmail.com>
2023-09-16 21:44:00 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+let apiKey = "";
+let searchTerm = "";
+
+const searchButton = document.getElementById("search-btn");
+
+searchButton.addEventListener("click", () => {
+    let apiKeyValue = document.getElementById("api-input").value;
+    let searchvalue = document.getElementById("movie-name-input").value;
+    console.log(apiKeyValue);
+    if(apiKeyValue !== ""){
+        apiKey = apiKeyValue;
+    }else{
+        alert("Plz Enter valid API");
+    }
+    if()
+
+
+})
