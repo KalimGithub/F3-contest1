b27969181e32d01303bbd83e0137f56b0a7266e5
KalimGithub <sk22kalim@gmail.com>
2023-09-16 21:58:41 +0530

    first commit

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ searchButton.addEventListener("click", () => {
     let apiKeyValue = document.getElementById("api-input").value;
     let searchvalue = document.getElementById("movie-name-input").value;
     console.log(apiKeyValue);
-    if(apiKeyValue !== ""){
+    if(apiKeyValue !== "") {
         apiKey = apiKeyValue;
-    }else{
+    } else {
         alert("Plz Enter valid API");
     }
-    if()
+    if( searchvalue.length < 3 ) {
+        document.querySelector(".warning").innerHTML = "";
+        const warning = document.querySelector(".warning");
+        warning.innerText = "Search term should be at least of 3 characters";
+    } else {
+        searchTerm = searchvalue;
+    }
 
+    if (apiKeyValue !== "" && searchvalue.length >= 3) {
+        const warning = document.querySelector(".warning");
+        warning.innerText = "";
+        fetchData();
+    }
+});
 
-})
+function fetchData() {
+    const endPoint = `https://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`;
+    document.querySelector(".cards-container").innerHTML = "";
+    
+}
